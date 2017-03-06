772f7af641d73253172de5f5b62fb595884bea8d
Igor Dmitriev <igor.s.dmitriev@gmail.com>
2017-03-06 00:56:10 -0800

    Updates

--- src/lib/colors.js
+++
@@ -1,7 +0,0 @@
-export const COLORS = {
-  cash: '#2D7DBC',
-  gold: '#FFD034',
-  bonds: '#DB3A34',
-  options: '#4A4A4A',
-  stocks: '#037365'
-};
--- src/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import risk from './riskReducer';
-
-export default combineReducers({
-  risk
-});
